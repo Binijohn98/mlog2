42280ce99ff694484bc2b7284a3d92d601edfc1d
Kalpesh Deshmukh <binijohn98@gmail.com>
2023-01-06 23:59:54 +0000

    First commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Post;
+
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function show(Post $post){
+
+        return view('blog-post',['post'=>$post]);
+    }
+
+    public function create(Post $post){
+
+        return view('admin.posts.create');
+    }
+}
+ 
